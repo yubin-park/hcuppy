efeeb6381349975ab820324d18056f75b821a338
Daniel Weitzenfeld <dweitzenfeld@gmail.com>
2019-12-16 16:41:28 -0600

    rename test files so they are easily discoverable; pep8

--- setup.py
+++ setup.py
@@ -4,22 +4,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(packages=find_packages(),
-    name="hcuppy",
-    version="0.0.7",
-    description="hcuppy is a Python implementation of HCUP Tools and Software",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Yubin Park",
-    author_email="yubin.park@gmail.com",
-    url="https://github.com/yubin-park/hcuppy",
-    license="Apaceh 2.0", 
-    install_requires = [],
-    include_package_data=True,
-    package_data={"": ["*.txt", "*.csv"]},
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent"
-    ])
-
-
+      name="hcuppy",
+      version="0.0.7",
+      description="hcuppy is a Python implementation of HCUP Tools and Software",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author="Yubin Park",
+      author_email="yubin.park@gmail.com",
+      url="https://github.com/yubin-park/hcuppy",
+      license="Apache 2.0",
+      install_requires=[],
+      include_package_data=True,
+      package_data={"": ["*.txt", "*.csv"]},
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: Apache Software License",
+          "Operating System :: OS Independent"
+      ])
